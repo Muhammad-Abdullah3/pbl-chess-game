5991d9113cc5108b5f34990bb19c34a5e4fcfd86
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2023-12-12 14:42:43 +0500

    new file:   board_arr.cpp

---
+++ board_arr.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    char chess_board[8][8]={' '};
+    //White pawns
+    for(int i=1;i==1;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            chess_board[i][j]='P';
+        }
+    }
+    //Displaying each index of the array in 8 by 8 form
+    for(int k=0;k<=7;k++)
+    {
+        for(int l=0;l<=7;l++)
+        {
+            cout<<setw(7)<<chess_board[k][l];
+        }
+        cout<<endl;
+        cout<<setw(4);
+    }
+    return 0;
+}
