751c8fd340563a26ec9df7f0ec3e6da018fbc544
Waheedakhtar902 <akhtarwaheed902@gmail.com>
2024-01-07 14:46:30 +0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,27 @@
 #include<string>
 #include<stdio.h>
 using namespace std;
-//Function to show instructions
-void instr()
+void new_game()
 {
-    system("CLS");
-	cout<<"Instructions:";
-	cout<<"1- Player 1 will play as white, and player 2 will play as black. \n2- Players will be chosen from already added player profiles.";
-	cout<<"\n3- White pieces are in uppercase alphabets.\n4-Black pieces are in lowercase alphabets.";
-	cout<<"\n5- Alphabet 'p/P' is used to for Pawns.\n6- Alphabet 'r/R' is used to for Rooks.";
-	cout<<"\n7- Alphabet 'n/N' is used to for Knights.\n8- Alphabet 'b/B' is used to for Bishops.";
-	cout<<"\n9- Alphabet 'q/Q' is used to for Queens.\n10- Alphabet 'k/K' is used to for Kings.";
-	cout<<"\n11- The last move will be shown in a sidebar.\n12-Every time a check takes place, an alarm will be given to the checked player.";
-	cout<<"\n13- At checkmate, the game will come to an end. \n 13- The stats are stored in a text file of both players in their profile.";
+	system("CLS");
+	cout<<"Chose player 1:";
+	cout<<"Chose Player 2:";
 }
-//Function to exit the game 
-void exit_game()
+// Function to add a new player profile
+void add_profile()
 {
-    system("CLS");
-	cout<<"You have successfully exited the game.";
-	exit(0);
+
 }
-//Home menu Function
+// Function to delete a players profile
+void del_profile()
+{
+
+}
+// Function to check players stats
+void chck_stats()
+{
+
+}
 void home_menu()
 {
 	int entr_num=0;
@@ -39,22 +39,41 @@ void home_menu()
     cin>>entr_num;
 	if (entr_num==1)
 {
-	system("CLS");
-	cout<<"Chose player 1:";
-	cout<<"Chose Player 2:";
+    new_game();
 }
 else if(entr_num==2)
 {
-	instr();
+	system("CLS");
+	cout<<"Instructions:";
+	cout<<"1- Player 1 will play as white, and player 2 will play as black. \n2- Players will be chosen from already added player profiles.";
+	cout<<"\n3- White pieces are in uppercase alphabets.\n4-Black pieces are in lowercase alphabets.";
+	cout<<"\n5- Alphabet 'p/P' is used to for Pawns.\n6- Alphabet 'r/R' is used to for Rooks.";
+	cout<<"\n7- Alphabet 'n/N' is used to for Knights.\n8- Alphabet 'b/B' is used to for Bishops.";
+	cout<<"\n9- Alphabet 'q/Q' is used to for Queens.\n10- Alphabet 'k/K' is used to for Kings.";
+	cout<<"\n11- The last move will be shown in a sidebar.\n12-Every time a check takes place, an alarm will be given to the checked player.";
+	cout<<"\n13- At checkmate, the game will come to an end. \n 13- The stats are stored in a text file of both players in their profile.";
 }
 else if (entr_num==6)
 {
-    exit_game();
+	system("CLS");
+	cout<<"You have successfully exited the game.";
+	exit(0);
+}
+else if (entr_num==3)
+{
+	add_profile();
+}
+else if (entr_num==4)
+{
+	del_profile();
+}
+else if (entr_num==5)
+{
+	chck_stats();
 }
 }
-//Main Game Function
+
 int main()
 {
-    home_menu();
     return 0;
 }
