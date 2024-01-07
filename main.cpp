acc988dc68fac5d483ffd55a449ddc6361b4ffbb
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-01-07 15:06:10 +0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,153 @@
 #include<string>
 #include<stdio.h>
 using namespace std;
+char chess_board[8][8] = {' '};
+//New game initializing function
 void new_game()
 {
 	system("CLS");
 	cout<<"Chose player 1:";
 	cout<<"Chose Player 2:";
+}
+//Setting board pieces for the first time in array
+void board_set()
+{
+    //White pawns
+    for(int i=1;i==1;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            chess_board[i][j]='P';
+        }
+    }
+    //Black Pawns
+    for(int i=6;i==6;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            chess_board[i][j]='p';
+        }
+    }
+    //white rook
+    for (int i=0;i==0;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==0||j==7)
+			{
+    		 chess_board[i][j]='R';
+			}
+
+		}
+	}
+	//Black rook.
+    for (int i=7;i==7;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==0||j==7)
+			{
+    		    chess_board[i][j]='r';
+			}
+
+		}
+	}
+	//white Knight.
+	for (int i=0;i==0;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==1||j==6)
+			{
+    		    chess_board[i][j]='N';
+			}
+
+		}
+	}
+	//Black knight
+	for (int i=7;i==7;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==1||j==6)
+			{
+    		    chess_board[i][j]='n';
+			}
+
+		}
+	}
+	//White Bishop.
+	for (int i=0;i==0;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==2||j==5)
+			{
+    		    chess_board[i][j]='B';
+			}
+
+		}
+	}
+	//Black bishop
+	  for (int i=7;i==7;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==2||j==5)
+			{
+    		    chess_board[i][j]='b';
+			}
+
+		}
+	}
+	//White Queen
+	for (int i=0;i==0;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==3)
+			{
+    		    chess_board[i][j]='Q';
+			}
+
+		}
+	}
+	//Black queen
+	for (int i=7;i==7;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==3)
+			{
+    		    chess_board[i][j]='q';
+			}
+
+		}
+	}
+	//White king
+	for (int i=0;i==0;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==4)
+			{
+    		    chess_board[i][j]='K';
+			}
+
+		}
+	}
+	//black King
+	  for (int i=7;i==7;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if (j==4)
+			{
+    		    chess_board[i][j]='k';
+			}
+
+		}
+	}
 }
 //Function to show instructions
 void instr()
