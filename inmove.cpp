26ca1085175dd4d33d61ccab9ac84cea4e8b53e8
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-01-16 19:02:56 +0500

    modified:   inmove.cpp modified:   inmove.exe

--- inmove.cpp
+++ inmove.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void inMove(string &,int &);
 int main()
 {
-    int moveNo =2;
+    int moveNo =1;
     string move;
     inMove(move,moveNo);
     return 0;
@@ -14,32 +14,37 @@ void inMove(string & move,int & moveNo)
     if(moveNo%2==1)
     {
         cout<<"White please enter your Move: ";
-        cin>>move;
-        if(move.size()!=5)//including the null character
+		cin>>move;
+        //
+        if(move.length()!=4)//including the null character
         {//calling the function again
-            cout<<"Input is not in notation.";
+            cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<move;
             inMove(move,moveNo);
         }
         for(int i=0;i<4;i+2)
         {
             if((move.at(i)<'A'||move.at(i)>'H')||(move.at(i)<'a'||move.at(i)>'h'))
             {
-
+                cout<<"Wrong Notation 2";
                 inMove(move,moveNo);
             }
         }
-        for(int i=1;i==1||i==3;i+=2)
+        for(int i=1;i<4;i+=2)
         {
             if(move.at(i)<'1'||move.at(i)>'8')
+            {
+                cout<<"Wrong Notation 3";
                 inMove(move,moveNo);
+            }
         }
+        cout<<"Right Notation";
     }
     else
     {
         cout<<"Black please enter your Move: ";
         cin>>move;
         int moveLength = move.length();
-        if(moveLength!=4);//not including the null character
+        if(moveLength!=4)//not including the null character
         {//calling the function again
             cout<<"Wrong Notation";
             inMove(move,moveNo);
